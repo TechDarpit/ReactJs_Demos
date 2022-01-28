15aaca4e0506676fce003db975309a15a1411baf
Darpit Makwana <79750384+TechDarpit@users.noreply.github.com>
2022-01-28 22:37:51 +0530

    Add files via upload

---
+++ 3 User List/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, Fragment } from "react";
+import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { id: Math.random().toString(), name: uName, age: uAge },
+      ];
+    });
+  };
+
+  return (
+    <Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </Fragment>
+  );
+}
+
+export default App;
